f3748b6720f5cb0b94f07bbe481b1f108fa01826
xavier-kong <kongxianchengxavier@gmail.com>
2021-12-04 16:47:40 +0800

    ok I think I've configured it!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
-import '../../styles/globals.css'
-import type { AppProps } from 'next/app'
 import React, { useEffect } from 'react'
+import '../../styles/globals.css'
 import PropTypes from 'prop-types'
 import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { CacheProvider } from '@emotion/react'
+import { CacheProvider, EmotionCache } from '@emotion/react'
 import theme from '../theme'
 import createEmotionCache from '../createEmotionCache'
 
 const clientSideEmotionCache = createEmotionCache()
 
-const MyApp = (props) => {
+const MyApp = function MyApp(props: {
+    // change later
+    Component: any
+    emotionCache?: EmotionCache | undefined
+    pageProps: any
+}) {
     const {
         Component,
         emotionCache = clientSideEmotionCache,
@@ -44,6 +48,7 @@ const MyApp = (props) => {
 }
 
 MyApp.propTypes = {
+    // fix later :)
     Component: PropTypes.elementType.isRequired,
     emotionCache: PropTypes.object,
     pageProps: PropTypes.object.isRequired,
