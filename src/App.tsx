80dca013990e9b40626196427642c4f37fb4aa50
xavier-kong <kongxianchengxavier@gmail.com>
2021-12-31 17:06:07 +0800

    this machine does not stop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-// import SideMenu from './components/SideMenu';
+import SideMenu from './components/SideMenu';
 import Navbar from './components/Navbar';
 
-const App = () => <Navbar />;
+// const App = () => <Navbar />;
+const App = () => (
+    <>
+        <Navbar />
+        <SideMenu />
+    </>
+);
 
 export default App;
